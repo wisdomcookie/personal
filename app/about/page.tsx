f43ffb398d95905029a31b32bd7fb67ba19b316a
Nathan_Dinh <dinhtrongnhan2015@gmail.com>
2023-06-20 19:44:52 -0400

    Update styling

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -14,11 +14,11 @@ export default function About() {
           <li>Other than coding, I  also like reading, cooking and video games</li>
           <li>I also work out, mostly to stay sane</li>
           <li>Non-CS academic interests include physics and policymaking</li>
-          <li>I tried (and failed) to publish a novel in 8th grade.</li>
+          <li>I tried to publish a novel in 8th grade.</li>
           <li>I used to know how to play guitar</li>
           <li>You may want my <a href="https://drive.google.com/file/d/1xO1_wLNlJoyNQBvnhSB7pOG9ofZNNEd6/view?usp=sharing" target='_blank' className='font-bold'>Resume</a>, <a href="https://linkedin.com/in/nathan-dt" target='_blank' className='font-bold'>LinkedIn</a> or <a href="https://github.com/wisdomcookie" target='_blank' className='font-bold'>GitHub</a>.</li>
         </ul>
-        <p className='mt-6'>
+        <p className='mt-6 text-l'>
         I don&apos;t really know what else you want to know so here are some assorted favorites:
         </p>
         <ul className='mt-3 space-y-1 bg-slate-600 p-4 rounded'>
@@ -35,8 +35,7 @@ export default function About() {
             <span className="font-bold">Color</span>: Blue
           </li>
           <li>
-            <span className="font-bold">Movie</span>: Interstellar. Or Stuck in Love. Or Set it Up, I like a lot of rom-coms. Picking favorites is hard,
-            have a picture of the cylindrical Santa Claus my sister gave me instead:
+            <span className="font-bold">Movie</span>: Interstellar. Or Stuck in Love. Or Set it Up. I like a lot of rom-coms.
           </li>
         </ul>
         <SantaPic/>
